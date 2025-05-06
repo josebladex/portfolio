7335cb2caa430921134913c44f14d6eafbb1c5b9
Jose Plata <josebladex@gmail.com>
2025-05-06 13:22:09 -0500

    update portfolio data

--- data/index.ts
+++ data/index.ts
@@ -1,9 +1,17 @@
-
-
 export const projects = {
   es: [
     {
       id: 1,
+      title: 'Rick & Morty: Juego de Prueba Backend',
+      des: '¡Una novela visual fan-made de Rick and Morty con historias ramificadas, decisiones del jugador y aventuras caóticas!',
+      img1: '/projects/22.png',
+      img2: '/projects/23.png',
+      iconLists: ['/next.svg', '/ts.svg', '/ravendb.svg', '/dotnet.svg'],
+      link: 'https://github.com/josebladex/Rick-Morty_Backend-Test-Game',
+      year: 'May/2025 - En Desarrollo'
+    },
+    {
+      id: 2,
       title: 'Pixl Technical Test - Plataforma de Eventos',
       des: 'Plataforma para eventos con autenticación y pagos integrados.',
       img1: '/projects/20.png',
@@ -16,70 +24,70 @@ export const projects = {
         '/docker-svgrepo-com.svg'
       ],
       link: 'https://github.com/josebladex/pixl-test',
-      year: '2025'
+      year: 'Mar/2025 (2 días)'
     },
     {
-      id: 2,
+      id: 3,
       title: 'SCHNEIDER ELECTRIC Hackathon 2025',
       des: 'Ciencia de datos para optimizar procesos industriales.',
       img1: '/projects/19.png',
       img2: '/projects/19.png',
       iconLists: ['/bg/python-5.svg'],
       link: 'https://nuwe.io/es/hackathons/schneider-electric-iberian-2025?section=challenges',
-      year: '2025'
+      year: 'Mar/2025 (3 días)'
     },
     {
-      id: 3,
+      id: 4,
       title: 'SchedSyncAI',
       des: 'SaaS para sincronizar agendas con inteligencia artificial.',
       img1: '/projects/18.png',
       img2: '/projects/17.png',
       iconLists: ['/docker-svgrepo-com.svg'],
-      link: '#',
-      year: '?'
+      link: '',
+      year: 'Feb/2025 - En Desarrollo'
     },
     {
-      id: 4,
+      id: 5,
       title: 'Logistica MRMS',
       des: 'Gestión de transporte y logística con colaboración empresarial.',
       img1: '/projects/14.png',
       img2: '/projects/13.png',
       iconLists: ['/re.svg', '/docker-svgrepo-com.svg', '/bg/python-5.svg'],
       link: 'https://logisticamrms.com/',
-      year: '2025'
+      year: 'Dic/2025 - Feb/2025'
     },
     {
-      id: 5,
+      id: 6,
       title: 'Hackathon: The Hack is ON',
       des: 'Solución Full Stack para un reto técnico.',
       img1: '/projects/16.png',
       img2: '/projects/15.png',
       iconLists: ['/re.svg', '/java.svg', '/spring.svg'],
       link: 'https://nuwe.io/es/hackathons/the-hack-is-on',
-      year: '2025'
+      year: 'Feb/2025 (2 días)'
     },
     {
-      id: 6,
+      id: 7,
       title: 'BOOST HACKATHON',
       des: 'Ciencia de datos y machine learning para optimización empresarial.',
       img1: '/projects/12.png',
       img2: '/projects/11.png',
       iconLists: ['/bg/python-5.svg'],
       link: 'https://nuwe.io/es/hackathons/boost-hackathon',
-      year: '2024'
+      year: 'Dic/2025 - Ene/2025'
     },
     {
-      id: 7,
+      id: 8,
       title: 'UTOPIA',
       des: 'Explorador de exoplanetas para el desafío NASA Space Apps.',
       img1: '/projects/9.png',
       img2: '/projects/10.png',
       iconLists: ['/three.svg', '/next.svg', '/bg/python-5.svg', '/ts.svg'],
       link: 'https://utopia-exoplanet-quest.vercel.app/',
-      year: '2024'
+      year: 'Oct/2024 - (2 días)'
     },
     {
-      id: 8,
+      id: 9,
       title: 'Mundo Pie',
       des: 'Aplicación web para marketing y CRM clínico.',
       img1: '/projects/1.png',
@@ -92,20 +100,20 @@ export const projects = {
         '/bg/google-calendar-svgrepo-com.svg'
       ],
       link: 'https://mundopie.cl/',
-      year: '2023'
+      year: 'Ene/2022 - Dic/2023'
     },
     {
-      id: 9,
+      id: 10,
       title: 'Electromagnetismo',
       des: 'Programas para analizar experimentos de electromagnetismo.',
       img1: '/projects/2.png',
       img2: '/projects/3.png',
       iconLists: ['/bg/python-5.svg', '/bg/arduino-1.svg'],
       link: 'https://github.com/josebladex/Recursos-Python-Fisica-II',
-      year: '2022'
+      year: 'Ago/2022 - (15 días)'
     },
     {
-      id: 10,
+      id: 11,
       title: 'EnriqueLeeApp',
       des: 'Plataforma de red social con soporte móvil.',
       img1: '/projects/4.png',
@@ -116,10 +124,10 @@ export const projects = {
         '/bg/firebase-svgrepo-com.svg'
       ],
       link: 'https://github.com/josebladex/EnriqueLeeApp',
-      year: '2022'
+      year: '2021 - (1 mes)'
     },
     {
-      id: 11,
+      id: 12,
       title: 'Gotarget',
       des: 'Proyecto inicial de landing page para marketing.',
       img1: '/projects/6.png',
@@ -136,6 +144,16 @@ export const projects = {
   en: [
     {
       id: 1,
+      title: 'Rick & Morty: Backend Test Game',
+      des: 'A fan-made Rick and Morty visual novel game featuring branching storylines, player choices, and chaotic adventures!',
+      img1: '/projects/22.png',
+      img2: '/projects/23.png',
+      iconLists: ['/next.svg', '/ts.svg', '/ravendb.svg', '/dotnet.svg'],
+      link: 'https://github.com/josebladex/Rick-Morty_Backend-Test-Game',
+      year: 'May/2025 - In Development'
+    },
+    {
+      id: 2,
       title: 'Pixl Technical Test - Event Management Platform',
       des: 'Platform for events with authentication and integrated payments.',
       img1: '/projects/20.png',
@@ -148,70 +166,70 @@ export const projects = {
         '/docker-svgrepo-com.svg'
       ],
       link: 'https://github.com/josebladex/pixl-test',
-      year: '2025'
+      year: 'Mar/2025 (2 days)'
     },
     {
-      id: 2,
+      id: 3,
       title: 'SCHNEIDER ELECTRIC Hackathon 2025',
       des: 'Data science to optimize industrial processes.',
       img1: '/projects/19.png',
       img2: '/projects/19.png',
       iconLists: ['/bg/python-5.svg'],
       link: 'https://nuwe.io/es/hackathons/schneider-electric-iberian-2025?section=challenges',
-      year: '2025'
+      year: 'Mar/2025 (3 days)'
     },
     {
-      id: 3,
+      id: 4,
       title: 'SchedSyncAI',
       des: 'SaaS to synchronize schedules with artificial intelligence.',
       img1: '/projects/18.png',
       img2: '/projects/17.png',
       iconLists: ['/docker-svgrepo-com.svg'],
       link: '#',
-      year: '?'
+      year: 'Feb/2025 - In Development'
     },
     {
-      id: 4,
+      id: 5,
       title: 'Logistica MRMS',
       des: 'Transportation and logistics management with business collaboration.',
       img1: '/projects/14.png',
       img2: '/projects/13.png',
       iconLists: ['/re.svg', '/docker-svgrepo-com.svg', '/bg/python-5.svg'],
       link: 'https://logisticamrms.com/',
-      year: '2025'
+      year: 'Dec/2025 - Feb/2025'
     },
     {
-      id: 5,
+      id: 6,
       title: 'Hackathon: The Hack is ON',
       des: 'Full Stack solution for a technical challenge.',
       img1: '/projects/16.png',
       img2: '/projects/15.png',
       iconLists: ['/re.svg', '/java.svg', '/spring.svg'],
       link: 'https://nuwe.io/es/hackathons/the-hack-is-on',
-      year: '2025'
+      year: 'Feb/2025 (2 days)'
     },
     {
-      id: 6,
+      id: 7,
       title: 'BOOST HACKATHON',
       des: 'Data science and machine learning for business optimization.',
       img1: '/projects/12.png',
       img2: '/projects/11.png',
       iconLists: ['/bg/python-5.svg'],
       link: 'https://nuwe.io/es/hackathons/boost-hackathon',
-      year: '2024'
+      year: 'Dec/2025 - Jan/2025'
     },
     {
-      id: 7,
+      id: 8,
       title: 'UTOPIA',
       des: 'Exoplanet explorer for NASA Space Apps challenge.',
       img1: '/projects/9.png',
       img2: '/projects/10.png',
       iconLists: ['/three.svg', '/next.svg', '/bg/python-5.svg', '/ts.svg'],
       link: 'https://utopia-exoplanet-quest.vercel.app/',
-      year: '2024'
+      year: 'Oct/2024 - (2 days)'
     },
     {
-      id: 8,
+      id: 9,
       title: 'Mundo Pie',
       des: 'Web application for marketing and clinical CRM.',
       img1: '/projects/1.png',
@@ -224,20 +242,20 @@ export const projects = {
         '/bg/google-calendar-svgrepo-com.svg'
       ],
       link: 'https://mundopie.cl/',
-      year: '2023'
+      year: 'Jan/2022 - Dec/2023'
     },
     {
-      id: 9,
+      id: 10,
       title: 'Electromagnetism',
       des: 'Programs to analyze electromagnetism experiments.',
       img1: '/projects/2.png',
       img2: '/projects/3.png',
       iconLists: ['/bg/python-5.svg', '/bg/arduino-1.svg'],
       link: 'https://github.com/josebladex/Recursos-Python-Fisica-II',
-      year: '2022'
+      year: 'Aug/2022 - (15 days)'
     },
     {
-      id: 10,
+      id: 11,
       title: 'EnriqueLeeApp',
       des: 'Social network platform with mobile support.',
       img1: '/projects/4.png',
@@ -248,10 +266,10 @@ export const projects = {
         '/bg/firebase-svgrepo-com.svg'
       ],
       link: 'https://github.com/josebladex/EnriqueLeeApp',
-      year: '2022'
+      year: '2021 - (1 month)'
     },
     {
-      id: 11,
+      id: 12,
       title: 'Gotarget',
       des: 'Initial landing page project for marketing.',
       img1: '/projects/6.png',
